10f03e1bca3478a3a409bebfe7c8e55a6bbc8176
Sam Thorold <sam.thorold@gmail.com>
2017-08-16 00:03:02 +0100

    Global base templates

--- invoice/models.py
+++ invoice/models.py
@@ -5,20 +5,33 @@ class WorkType(models.Model):
 
 	name = models.CharField(max_length=200)
 
+	def __str__(self):
+		return self.name
+
 class Invoice(models.Model):
 
 	payee = models.ForeignKey('contacts.Contact')
+
+	def __str__(self):
+		return self.payee.name
 
 class InvoiceLine(models.Model):
 
 	invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE)
 	work_type = models.ForeignKey(WorkType)
 	patient = models.ForeignKey('contacts.Contact')
+	quantity = models.IntegerField()
 	fee = models.IntegerField()
 	billable = models.BooleanField()
+
+	def __str__(self):
+		return "{}: {}".format(self.work_type.name, self.patient.name)
 
 class Payment(models.Model):
 
 	invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE)
 	date = models.DateField()
 	amount = models.IntegerField()
+
+	def __str__(self):
+		return "{}: {}".format(self.invoice.payee.name, self.date)
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', auth_views.login, name='login'),
+    url(r'^accounts/logout/$', auth_views.logout, name='logout'),
     url(r'^invoice/', include('invoice.urls')),
     url(r'^contacts/', include('contacts.urls')),
 ]
