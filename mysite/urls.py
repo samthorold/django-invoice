c94eb699acdb8ad5baaf46d17543a3f44b173508
Sam Thorold <sam.thorold@gmail.com>
2018-04-12 11:10:38 +0100

    root url redirect to invoice home

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,8 +17,10 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
+from . import views
 
 urlpatterns = [
+    url(r'^$', views.home, name='home'),
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/login/$', auth_views.login, name='login'),
     url(r'^accounts/logout/$', auth_views.logout, name='logout'),
